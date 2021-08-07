40cbd18f374bb7013a2d0d7b86348ee8133a5788
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-07 23:37:24 +0530

    CREATE: torus(ring shape)

--- main.js
+++ main.js
@@ -15,4 +15,21 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene, camera)
+renderer.render(scene, camera);
+
+
+const geometry = new THREE.TorusGeometry(13, 3, 20, 100);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xFF6347,
+    wireframe: true
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera)
+}
+
+animate()
